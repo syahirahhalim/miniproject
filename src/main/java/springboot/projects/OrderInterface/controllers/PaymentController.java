ee5fa24d534d04133a67ae064ed09086c32429b9
syahirahhalim <syahawesome@gmail.com>
2022-05-16 12:05:59 +0800

    updated

--- src/main/java/springboot/projects/OrderInterface/controllers/PaymentController.java
+++ src/main/java/springboot/projects/OrderInterface/controllers/PaymentController.java
@@ -1,13 +1,29 @@
 package springboot.projects.OrderInterface.controllers;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springboot.projects.OrderInterface.models.User;
 
 @Controller
 public class PaymentController {
 
     @RequestMapping("/payment")
-    public String getPayment() {
+    public String getPayment(Model model) {
+        return "payment";
+    }
+
+    @RequestMapping(name="/confirm", method=RequestMethod.POST,
+            consumes="application/x-www-form-urlencoded",
+            produces="text/html"
+    )
+    public String confirmPayment(@ModelAttribute User user, Model model) {
+        // TODO: Create user if confirmation done
+
+        model.addAttribute("confirmation", true);
         return "payment";
     }
 }
--- src/main/java/springboot/projects/OrderInterface/controllers/ProductsController.java
+++ src/main/java/springboot/projects/OrderInterface/controllers/ProductsController.java
@@ -1,13 +1,55 @@
 package springboot.projects.OrderInterface.controllers;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springboot.projects.OrderInterface.models.Products;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ProductsController {
 
+    @Autowired
+    private DataSource dataSource;
+
     @RequestMapping("/")
     public String getMenu() {
+        List<Products> products = null;
+        try {
+//            products = getProducts();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        model.addAttribute(products);
         return "products";
+    }
+
+    private List<Products> getProducts() throws SQLException {
+        if (dataSource == null) return null;
+
+        List<Products> productList = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection()) {
+            try(PreparedStatement statement = connection.prepareStatement("SELECT * FROM Products;")) {
+                try(ResultSet resultSet = statement.executeQuery()) {
+                    while (resultSet.next()) {
+                        productList.add(new Products(
+                                resultSet.getInt("id"),
+                                resultSet.getString("name"),
+                                resultSet.getInt("price"),
+                                resultSet.getString("status"),
+                                resultSet.getString("createdAt")));
+                    }
+                }
+            }
+        }
+        return productList;
     }
 }
